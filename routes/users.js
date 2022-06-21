ce87a3f100c078c3ade31b0a3b303a085f25b899
Sneha Mahajan <sneh.km@gmail.com>
2022-06-21 18:23:09 -0400

    Add route get homepage and to post (submit) poll creation request.

--- routes/users.js
+++ routes/users.js
@@ -5,21 +5,65 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
-module.exports = (db) => {
+module.exports = (database) => {
+  // router.get("/", (req, res) => {
+  //   database
+  //     .query(`SELECT * FROM users;`)
+  //     .then((data) => {
+  //       const users = data.rows;
+  //       res.json({ users });
+  //     })
+  //     .catch((err) => {
+  //       res.status(500).json({ error: err.message });
+  //     });
+  // });
+
+  // Home page to begin the poll creation
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+    const templateVars = {};
+    res.render("index", templateVars);
   });
+
+  // Submit a new pole
+  router.post("/poll", (req, res) => {
+    const pollData = req.body;
+
+    const shareID = generateUniqueRandomPollId();
+    const adminID = generateUniqueRandomPollId();
+    const links = {
+      shareLink: `/poll/${shareID}`,
+      adminLink: `/poll/${adminID}`,
+    };
+
+    database.addPoll(pollData, links)
+    .then(poll => {
+      if (!poll) {
+        res.send({error: "error"});
+        return;
+      }
+      req.session.userId = poll.id;
+      res.render("vote", links);
+    })
+    .catch(e => res.send(e));
+  });
+
+
+  // Helper funciton to generate random ID for links
+  const generateUniqueRandomPollId = () => {
+    let id = "";
+    let strLen = 6;
+    const chars =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    for (let i = 0; i < strLen; i++) {
+      const randIndex = Math.floor(Math.random() * chars.length);
+      id = id.concat(chars[randIndex]);
+    }
+    return id;
+  };
+
   return router;
 };
