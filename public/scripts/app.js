bcda3936b6bc6e248e9c09ddc6cd86469f15ac37
VincentHChoy <vhchoy@ualberta.ca>
2022-06-21 15:20:23 -0600

    adds canvasJS example for how graphs should be displayed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,50 @@
 // Client facing scripts here
+const dummyData = [
+  { label: "apple", y: 10 },
+  { label: "orange", y: 15 },
+  { label: "banana", y: 25 },
+  { label: "mango", y: 30 },
+  { label: "grape", y: 28 },
+];
+
+$(document).ready(function () {
+  $(function () {
+    $(".sortable").sortable();
+  });
+
+
+  var options = {
+    title: {
+      text: "Desktop OS Market Share in 2017",
+    },
+    subtitles: [
+      {
+        text: "As of November, 2017",
+      },
+    ],
+    animationEnabled: true,
+    data: [
+      {
+        type: "pie",
+        startAngle: 40,
+        toolTipContent: "<b>{label}</b>: {y}%",
+        showInLegend: "true",
+        legendText: "{label}",
+        indexLabelFontSize: 16,
+        indexLabel: "{label} - {y}%",
+        dataPoints: [
+          { y: 48.36, label: "Windows 7" },
+          { y: 26.85, label: "Windows 10" },
+          { y: 1.49, label: "Windows 8" },
+          { y: 6.98, label: "Windows XP" },
+          { y: 6.53, label: "Windows 8.1" },
+          { y: 2.45, label: "Linux" },
+          { y: 3.32, label: "Mac OS X 10.12" },
+          { y: 4.03, label: "Others" },
+        ],
+      },
+    ],
+  };
+  $("#chartContainer").CanvasJSChart(options);
+
+});
