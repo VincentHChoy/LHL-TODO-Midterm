fdfa434a0cdd9fe66f4f31d63f05db3a04c9cd74
MeganTherion <meganhutt@hotmail.com>
2022-06-22 19:25:20 +0000

    add seeds/01_users

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -8,6 +8,16 @@ VALUES ('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmv
 INSERT INTO polls (created_by, question_text, created_at, end_date, active)
 VALUES (1, 'Where should we eat tonight?', CURRENT_TIME, 2022-09-22, TRUE),
 (2, 'What movie should we watch this weekend?', CURRENT_TIME, 2022-09-22, TRUE),
-(4, 'Which TV show is best?', CURRENT_TIME, 2022-09-22, TRUE),
-(3, 'Which is the best vacation spot?', CURRENT_TIME, 2022-09-22, TRUE),
+(3, 'Which TV show is best?', CURRENT_TIME, 2022-09-22, TRUE),
+(4, 'Which is the best vacation spot?', CURRENT_TIME, 2022-09-22, TRUE),
+
+INSERT INTO options (poll_id, option_text)
+VALUES (1, 'Sunshine Diner'),
+(1, 'Jam Cafe'),
+(1, 'Au Comptoir'),
+(1, 'Sophies Cosmic Cafe),
+(2, 'Terminator 2'),
+(2, 'First Blood'),
+(2, 'Tampopo'),
+(2, 'Enter The Dragon')
 
